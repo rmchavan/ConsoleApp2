42404666ee3d22cbc97a81a57c63aa553501640c
rmchavan <rameshvar123@gmail.com>
2025-02-08 06:43:10 +0530

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,22 @@
+﻿namespace ConsoleApp2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string str = "rameshwar";
+            char[] arr = str.ToCharArray();
+
+            for (int i = 0; i < str.Length; i++)
+            {
+                arr[i] = str[str.Length - 1 - i];
+            }
+            //for (int i = 1; i < 100; i++) 
+            //{
+
+            //}
+            Console.WriteLine(arr);
+            Console.ReadLine();
+        }
+    }
+}
